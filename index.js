31b8b15220aec2091accd6334cc76b30311ae9f4
Uilque Messias <uqmessias@gmail.com>
2019-07-13 18:18:36 -0300

    feat(prettier): install prettier and apply it to the code

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import { View } from 'react-native';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 import xmldom from 'xmldom';
 import resolveAssetSource from 'react-native/Libraries/Image/resolveAssetSource';
 
@@ -19,7 +19,7 @@ import Svg, {
   TSpan,
   Defs,
   Use,
-  Stop
+  Stop,
 } from 'react-native-svg';
 
 import * as utils from './utils';
@@ -40,7 +40,7 @@ const ACCEPTED_SVG_ELEMENTS = [
   'polygon',
   'polyline',
   'text',
-  'tspan'
+  'tspan',
 ];
 
 // Attributes from SVG elements that are mapped directly.
@@ -56,25 +56,48 @@ const RADIALG_ATTS = CIRCLE_ATTS.concat(['id', 'gradientUnits', 'fx', 'fy']);
 const STOP_ATTS = ['offset', 'stopColor'];
 const ELLIPSE_ATTS = ['cx', 'cy', 'rx', 'ry'];
 
-const TEXT_ATTS = ['fontFamily', 'fontSize', 'fontWeight', 'textAnchor']
+const TEXT_ATTS = ['fontFamily', 'fontSize', 'fontWeight', 'textAnchor'];
 
 const POLYGON_ATTS = ['points'];
 const POLYLINE_ATTS = ['points'];
 
 const USE_ATTS = ['href'];
 
-const COMMON_ATTS = ['id', 'fill', 'fillOpacity', 'stroke', 'strokeWidth', 'strokeOpacity', 'opacity',
-  'strokeLinecap', 'strokeLinejoin',
-  'strokeDasharray', 'strokeDashoffset', 'x', 'y', 'rotate', 'scale', 'origin', 'originX', 'originY',
-  'transform', 'clipPath', 'fillRule'];
+const COMMON_ATTS = [
+  'id',
+  'fill',
+  'fillOpacity',
+  'stroke',
+  'strokeWidth',
+  'strokeOpacity',
+  'opacity',
+  'strokeLinecap',
+  'strokeLinejoin',
+  'strokeDasharray',
+  'strokeDashoffset',
+  'x',
+  'y',
+  'rotate',
+  'scale',
+  'origin',
+  'originX',
+  'originY',
+  'transform',
+  'clipPath',
+  'fillRule',
+];
 
 let ind = 0;
 
 // https://developer.mozilla.org/en-US/docs/Web/SVG/Element/use#Attributes
 function fixXlinkHref(node) {
   if (node.attributes) {
-    const hrefAttr = Object.keys(node.attributes).find(a => node.attributes[a].name === 'href');
-    const legacyHrefAttr = Object.keys(node.attributes).find(a => node.attributes[a].name === 'xlink:href');
+    const hrefAttr = Object.keys(node.attributes).find(
+      a => node.attributes[a].name === 'href',
+    );
+    const legacyHrefAttr = Object.keys(node.attributes).find(
+      a => node.attributes[a].name === 'xlink:href',
+    );
 
     return node.attributes[hrefAttr || legacyHrefAttr].value;
   }
@@ -83,19 +106,22 @@ function fixXlinkHref(node) {
 
 function fixYPosition(y, node) {
   if (node.attributes) {
-    const fontSizeAttr = Object.keys(node.attributes).find(a => node.attributes[a].name === 'font-size');
+    const fontSizeAttr = Object.keys(node.attributes).find(
+      a => node.attributes[a].name === 'font-size',
+    );
     if (fontSizeAttr) {
-      return '' + (parseFloat(y) - parseFloat(node.attributes[fontSizeAttr].value));
+      return (
+        '' + (parseFloat(y) - parseFloat(node.attributes[fontSizeAttr].value))
+      );
     }
   }
   if (!node.parentNode) {
     return y;
   }
-  return fixYPosition(y, node.parentNode)
+  return fixYPosition(y, node.parentNode);
 }
 
 class SvgRenderer extends Component {
-
   constructor(props) {
     super(props);
 
@@ -138,17 +164,18 @@ class SvgRenderer extends Component {
   }
 
   componentWillUnmount() {
-    this.isComponentMounted = false
+    this.isComponentMounted = false;
   }
 
   async fetchSVGData(uri) {
-    let responseXML = null, error = null;
+    let responseXML = null,
+      error = null;
     try {
       const response = await fetch(uri);
       responseXML = await response.text();
     } catch (e) {
       error = e;
-      console.error("ERROR SVG", e);
+      console.error('ERROR SVG', e);
     } finally {
       if (this.isComponentMounted) {
         this.setState({ svgXmlData: responseXML }, () => {
@@ -187,25 +214,49 @@ class SvgRenderer extends Component {
           componentAtts.height = this.props.height;
         }
 
-        return <Svg key={i} {...componentAtts}>{children}</Svg>;
+        return (
+          <Svg key={i} {...componentAtts}>
+            {children}
+          </Svg>
+        );
       case 'g':
         componentAtts = this.obtainComponentAtts(node, G_ATTS);
-        return <G key={i} {...componentAtts}>{children}</G>;
+        return (
+          <G key={i} {...componentAtts}>
+            {children}
+          </G>
+        );
       case 'path':
         componentAtts = this.obtainComponentAtts(node, PATH_ATTS);
         if (this.props.fill) {
           componentAtts.fill = this.props.fill;
         }
-        return <Path key={i} {...componentAtts}>{children}</Path>;
+        return (
+          <Path key={i} {...componentAtts}>
+            {children}
+          </Path>
+        );
       case 'circle':
         componentAtts = this.obtainComponentAtts(node, CIRCLE_ATTS);
-        return <Circle key={i} {...componentAtts}>{children}</Circle>;
+        return (
+          <Circle key={i} {...componentAtts}>
+            {children}
+          </Circle>
+        );
       case 'rect':
         componentAtts = this.obtainComponentAtts(node, RECT_ATTS);
-        return <Rect key={i} {...componentAtts}>{children}</Rect>;
+        return (
+          <Rect key={i} {...componentAtts}>
+            {children}
+          </Rect>
+        );
       case 'line':
         componentAtts = this.obtainComponentAtts(node, LINE_ATTS);
-        return <Line key={i} {...componentAtts}>{children}</Line>;
+        return (
+          <Line key={i} {...componentAtts}>
+            {children}
+          </Line>
+        );
       case 'defs':
         return <Defs key={i}>{children}</Defs>;
       case 'use':
@@ -214,31 +265,63 @@ class SvgRenderer extends Component {
         return <Use key={i} {...componentAtts} />;
       case 'linearGradient':
         componentAtts = this.obtainComponentAtts(node, LINEARG_ATTS);
-        return <LinearGradient key={i} {...componentAtts}>{children}</LinearGradient>;
+        return (
+          <LinearGradient key={i} {...componentAtts}>
+            {children}
+          </LinearGradient>
+        );
       case 'radialGradient':
         componentAtts = this.obtainComponentAtts(node, RADIALG_ATTS);
-        return <RadialGradient key={i} {...componentAtts}>{children}</RadialGradient>;
+        return (
+          <RadialGradient key={i} {...componentAtts}>
+            {children}
+          </RadialGradient>
+        );
       case 'stop':
         componentAtts = this.obtainComponentAtts(node, STOP_ATTS);
-        return <Stop key={i} {...componentAtts}>{children}</Stop>;
+        return (
+          <Stop key={i} {...componentAtts}>
+            {children}
+          </Stop>
+        );
       case 'ellipse':
         componentAtts = this.obtainComponentAtts(node, ELLIPSE_ATTS);
-        return <Ellipse key={i} {...componentAtts}>{children}</Ellipse>;
+        return (
+          <Ellipse key={i} {...componentAtts}>
+            {children}
+          </Ellipse>
+        );
       case 'polygon':
         componentAtts = this.obtainComponentAtts(node, POLYGON_ATTS);
-        return <Polygon key={i} {...componentAtts}>{children}</Polygon>;
+        return (
+          <Polygon key={i} {...componentAtts}>
+            {children}
+          </Polygon>
+        );
       case 'polyline':
         componentAtts = this.obtainComponentAtts(node, POLYLINE_ATTS);
-        return <Polyline key={i} {...componentAtts}>{children}</Polyline>;
+        return (
+          <Polyline key={i} {...componentAtts}>
+            {children}
+          </Polyline>
+        );
       case 'text':
         componentAtts = this.obtainComponentAtts(node, TEXT_ATTS);
-        return <Text key={i} {...componentAtts}>{children}</Text>;
+        return (
+          <Text key={i} {...componentAtts}>
+            {children}
+          </Text>
+        );
       case 'tspan':
         componentAtts = this.obtainComponentAtts(node, TEXT_ATTS);
         if (componentAtts.y) {
-          componentAtts.y = fixYPosition(componentAtts.y, node)
+          componentAtts.y = fixYPosition(componentAtts.y, node);
         }
-        return <TSpan key={i} {...componentAtts}>{children}</TSpan>;
+        return (
+          <TSpan key={i} {...componentAtts}>
+            {children}
+          </TSpan>
+        );
       default:
         return null;
     }
@@ -252,11 +335,14 @@ class SvgRenderer extends Component {
     }
 
     Array.from(attributes).forEach(({ nodeName, nodeValue }) => {
-      Object.assign(styleAtts, utils.transformStyle({
-        nodeName,
-        nodeValue,
-        fillProp: this.state.fill
-      }));
+      Object.assign(
+        styleAtts,
+        utils.transformStyle({
+          nodeName,
+          nodeValue,
+          fillProp: this.state.fill,
+        }),
+      );
     });
 
     const componentAtts = Array.from(attributes)
@@ -264,8 +350,11 @@ class SvgRenderer extends Component {
       .map(utils.removePixelsFromNodeValue)
       .filter(utils.getEnabledAttributes(enabledAttributes.concat(COMMON_ATTS)))
       .reduce((acc, { nodeName, nodeValue }) => {
-        acc[nodeName] = (this.state.fill && nodeName === 'fill' && nodeValue !== 'none') ? this.state.fill : nodeValue
-        return acc
+        acc[nodeName] =
+          this.state.fill && nodeName === 'fill' && nodeValue !== 'none'
+            ? this.state.fill
+            : nodeValue;
+        return acc;
       }, {});
     Object.assign(componentAtts, styleAtts);
 
@@ -275,7 +364,7 @@ class SvgRenderer extends Component {
   inspectNode(node) {
     // Only process accepted elements
     if (!ACCEPTED_SVG_ELEMENTS.includes(node.nodeName)) {
-      return (<View key={ind++} />);
+      return <View key={ind++} />;
     }
 
     // Process the xml node
@@ -285,9 +374,9 @@ class SvgRenderer extends Component {
     // Recursive function.
     if (node.childNodes && node.childNodes.length > 0) {
       for (let i = 0; i < node.childNodes.length; i++) {
-        const isTextValue = node.childNodes[i].nodeValue
+        const isTextValue = node.childNodes[i].nodeValue;
         if (isTextValue) {
-          arrayElements.push(node.childNodes[i].nodeValue)
+          arrayElements.push(node.childNodes[i].nodeValue);
         } else {
           const nodo = this.inspectNode(node.childNodes[i]);
           if (nodo != null) {
@@ -306,22 +395,20 @@ class SvgRenderer extends Component {
         return null;
       }
 
-      const inputSVG = this.state.svgXmlData.substring(
-        this.state.svgXmlData.indexOf("<svg "),
-        (this.state.svgXmlData.indexOf("</svg>") + 6)
-      ).replace(/<!-(.*?)->/g, '');
+      const inputSVG = this.state.svgXmlData
+        .substring(
+          this.state.svgXmlData.indexOf('<svg '),
+          this.state.svgXmlData.indexOf('</svg>') + 6,
+        )
+        .replace(/<!-(.*?)->/g, '');
 
       const doc = new xmldom.DOMParser().parseFromString(inputSVG);
 
       const rootSVG = this.inspectNode(doc.childNodes[0]);
 
-      return (
-        <View style={this.props.style}>
-          {rootSVG}
-        </View>
-      );
+      return <View style={this.props.style}>{rootSVG}</View>;
     } catch (e) {
-      console.error("ERROR SVG", e);
+      console.error('ERROR SVG', e);
       return null;
     }
   }
@@ -335,7 +422,7 @@ SvgRenderer.propTypes = {
   source: PropTypes.any,
   fill: PropTypes.string,
   onLoad: PropTypes.func,
-  fillAll: PropTypes.bool
-}
+  fillAll: PropTypes.bool,
+};
 
 module.exports = SvgRenderer;
