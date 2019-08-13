66c294456aecb3af178bc530d63cd0c0535f58de
Uilque Messias <messias@work.co>
2019-08-12 22:59:06 -0300

    feat(example): Add some SVG samples on a list

---
+++ example/src/App.js
@@ -0,0 +1,61 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Fragment, useCallback } from 'react';
+import { SafeAreaView, FlatList, View, Text, StatusBar } from 'react-native';
+import SVGRenderer from 'react-native-svg-renderer';
+
+import SVGs from './assets/svgs';
+import styles, { Constants } from './styles';
+
+const App = () => {
+  const renderItem = useCallback(
+    ({ item: { title, xml }, index }) => (
+      <View
+        style={[
+          styles.itemContainer,
+          index < SVGs.length - 1 && styles.itemSeparator,
+        ]}
+        key={title}
+      >
+        <Text style={styles.itemTitle}>{title.toUpperCase()}</Text>
+        <View style={styles.svgContainer}>
+          <SVGRenderer
+            svgXmlData={xml}
+            height={Constants.cellSize}
+            width={Constants.cellSize}
+          />
+        </View>
+      </View>
+    ),
+    [SVGs],
+  );
+  const keyExtractor = useCallback(({ title }) => title, []);
+
+  return (
+    <Fragment>
+      <StatusBar
+        barStyle='light-content'
+        backgroundColor={Constants.colors.dark}
+      />
+      <SafeAreaView>
+        <FlatList
+          keyExtractor={keyExtractor}
+          ListHeaderComponent={
+            <Text style={styles.header}>{Constants.headerTitle}</Text>
+          }
+          style={styles.listContainer}
+          renderItem={renderItem}
+          data={SVGs}
+        />
+      </SafeAreaView>
+    </Fragment>
+  );
+};
+
+export default App;
