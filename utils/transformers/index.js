8ea3a71b2052fd22cf055d3c726a66b27ea8a65c
Uilque Messias <messias@work.co>
2019-11-21 16:56:27 -0300

    refactor(utils): extract `obtainComponentAtts` to utils

--- utils/transformers/index.js
+++ utils/transformers/index.js
@@ -30,3 +30,68 @@ export const transformStyle = ({ nodeName, nodeValue, fillProp }) => {
 
 export const getEnabledAttributes = enabledAttributes => ({ nodeName }) =>
   enabledAttributes.includes(dashToCamelCase(nodeName));
+
+const COMMON_ATTS = [
+  'id',
+  'fill',
+  'fillOpacity',
+  'stroke',
+  'strokeWidth',
+  'strokeOpacity',
+  'opacity',
+  'strokeLinecap',
+  'strokeLinejoin',
+  'strokeDasharray',
+  'strokeDashoffset',
+  'x',
+  'y',
+  'rotate',
+  'scale',
+  'origin',
+  'originX',
+  'originY',
+  'transform',
+  'clipPath',
+  'fillRule',
+];
+
+export const obtainComponentAtts = (
+  { attributes },
+  enabledAttributes,
+  fill,
+  fillAll,
+) => {
+  const styleAtts = {};
+  const attributeItems = Array.from(attributes);
+
+  if (fill && fillAll) {
+    styleAtts.fill = fill;
+  }
+
+  attributeItems.forEach(({ nodeName, nodeValue }) => {
+    Object.assign(
+      styleAtts,
+      transformStyle({
+        nodeName,
+        nodeValue,
+        fillProp: fill,
+      }),
+    );
+  });
+
+  const formatToCamelAndRemovePixels = nodeItem =>
+    removePixelsFromNodeValue(camelCaseNodeName(nodeItem));
+
+  const componentAtts = attributeItems
+    .map(formatToCamelAndRemovePixels)
+    .filter(getEnabledAttributes(enabledAttributes.concat(COMMON_ATTS)))
+    .reduce((acc, { nodeName, nodeValue }) => {
+      acc[nodeName] =
+        fill && nodeName === 'fill' && nodeValue !== 'none' ? fill : nodeValue;
+      return acc;
+    }, {});
+
+  Object.assign(componentAtts, styleAtts);
+
+  return componentAtts;
+};
