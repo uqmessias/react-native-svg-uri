a9d18d427a7d69bc7297cf869d1376c0bf29226d
Uilque Messias <messias@work.co>
2019-11-22 15:40:25 -0300

    refactor(svgRenderer): use extracted util to render every supported element

--- index.js
+++ index.js
@@ -23,7 +23,26 @@ import Svg, {
 } from 'react-native-svg';
 
 import * as utils from './utils/transformers';
-import { getFixedYPosition, getHrefValue, trimElementChilden } from './utils';
+import { trimElementChilden } from './utils';
+
+const tagsMap = {
+  ['circle']: Circle,
+  ['defs']: Defs,
+  ['ellipse']: Ellipse,
+  ['g']: G,
+  ['line']: Line,
+  ['linearGradient']: LinearGradient,
+  ['path']: Path,
+  ['polygon']: Polygon,
+  ['polyline']: Polyline,
+  ['radialGradient']: RadialGradient,
+  ['rect']: Rect,
+  ['stop']: Stop,
+  ['svg']: Svg,
+  ['text']: Text,
+  ['tspan']: TSpan,
+  ['use']: Use,
+};
 
 const ACCEPTED_SVG_ELEMENTS = [
   'svg',
@@ -43,26 +62,6 @@ const ACCEPTED_SVG_ELEMENTS = [
   'text',
   'tspan',
 ];
-
-// Attributes from SVG elements that are mapped directly.
-const SVG_ATTS = ['viewBox', 'width', 'height'];
-const G_ATTS = ['id'];
-
-const CIRCLE_ATTS = ['cx', 'cy', 'r'];
-const PATH_ATTS = ['d'];
-const RECT_ATTS = ['width', 'height'];
-const LINE_ATTS = ['x1', 'y1', 'x2', 'y2'];
-const LINEARG_ATTS = LINE_ATTS.concat(['id', 'gradientUnits', 'fx', 'fy']);
-const RADIALG_ATTS = CIRCLE_ATTS.concat(['id', 'gradientUnits', 'fx', 'fy']);
-const STOP_ATTS = ['offset', 'stopColor'];
-const ELLIPSE_ATTS = ['cx', 'cy', 'rx', 'ry'];
-
-const TEXT_ATTS = ['fontFamily', 'fontSize', 'fontWeight', 'textAnchor'];
-
-const POLYGON_ATTS = ['points'];
-const POLYLINE_ATTS = ['points'];
-
-const USE_ATTS = ['href'];
 
 let ind = 0;
 
@@ -135,133 +134,34 @@ class SvgRenderer extends Component {
   }
 
   createSVGElement(node, unTrimmedChildren) {
-    const children = trimElementChilden(unTrimmedChildren);
-    let componentAtts = {};
     const i = ind++;
+    const ElementTag = tagsMap[node.nodeName];
+    const element = utils.elementsMap[node.nodeName];
+
+    if (!ElementTag || !element) {
+      return null;
+    }
+
+    const children = trimElementChilden(unTrimmedChildren);
     const { fill, fillAll } = this.state;
     const { obtainComponentAtts } = utils;
+    const attrs = obtainComponentAtts(
+      node,
+      element.allowedAttributes,
+      fill,
+      fillAll,
+    );
+    const componentAtts = element.postProcessAttributes(
+      attrs,
+      this.props,
+      node,
+    );
 
-    switch (node.nodeName) {
-      case 'svg':
-        componentAtts = obtainComponentAtts(node, SVG_ATTS, fill, fillAll);
-        if (this.props.width) {
-          componentAtts.width = this.props.width;
-        }
-        if (this.props.height) {
-          componentAtts.height = this.props.height;
-        }
-
-        return (
-          <Svg key={i} {...componentAtts}>
-            {children}
-          </Svg>
-        );
-      case 'g':
-        componentAtts = obtainComponentAtts(node, G_ATTS, fill, fillAll);
-        return (
-          <G key={i} {...componentAtts}>
-            {children}
-          </G>
-        );
-      case 'path':
-        componentAtts = obtainComponentAtts(node, PATH_ATTS, fill, fillAll);
-        if (this.props.fill) {
-          componentAtts.fill = this.props.fill;
-        }
-        return (
-          <Path key={i} {...componentAtts}>
-            {children}
-          </Path>
-        );
-      case 'circle':
-        componentAtts = obtainComponentAtts(node, CIRCLE_ATTS, fill, fillAll);
-        return (
-          <Circle key={i} {...componentAtts}>
-            {children}
-          </Circle>
-        );
-      case 'rect':
-        componentAtts = obtainComponentAtts(node, RECT_ATTS, fill, fillAll);
-        return (
-          <Rect key={i} {...componentAtts}>
-            {children}
-          </Rect>
-        );
-      case 'line':
-        componentAtts = obtainComponentAtts(node, LINE_ATTS, fill, fillAll);
-        return (
-          <Line key={i} {...componentAtts}>
-            {children}
-          </Line>
-        );
-      case 'defs':
-        return <Defs key={i}>{children}</Defs>;
-      case 'use':
-        componentAtts = obtainComponentAtts(node, USE_ATTS, fill, fillAll);
-        componentAtts.href = getHrefValue(node);
-        return <Use key={i} {...componentAtts} />;
-      case 'linearGradient':
-        componentAtts = obtainComponentAtts(node, LINEARG_ATTS, fill, fillAll);
-        return (
-          <LinearGradient key={i} {...componentAtts}>
-            {children}
-          </LinearGradient>
-        );
-      case 'radialGradient':
-        componentAtts = obtainComponentAtts(node, RADIALG_ATTS, fill, fillAll);
-        return (
-          <RadialGradient key={i} {...componentAtts}>
-            {children}
-          </RadialGradient>
-        );
-      case 'stop':
-        componentAtts = obtainComponentAtts(node, STOP_ATTS, fill, fillAll);
-        return (
-          <Stop key={i} {...componentAtts}>
-            {children}
-          </Stop>
-        );
-      case 'ellipse':
-        componentAtts = obtainComponentAtts(node, ELLIPSE_ATTS, fill, fillAll);
-        return (
-          <Ellipse key={i} {...componentAtts}>
-            {children}
-          </Ellipse>
-        );
-      case 'polygon':
-        componentAtts = obtainComponentAtts(node, POLYGON_ATTS, fill, fillAll);
-        return (
-          <Polygon key={i} {...componentAtts}>
-            {children}
-          </Polygon>
-        );
-      case 'polyline':
-        componentAtts = obtainComponentAtts(node, POLYLINE_ATTS, fill, fillAll);
-        return (
-          <Polyline key={i} {...componentAtts}>
-            {children}
-          </Polyline>
-        );
-      case 'text':
-        componentAtts = obtainComponentAtts(node, TEXT_ATTS, fill, fillAll);
-        return (
-          <Text key={i} {...componentAtts}>
-            {children}
-          </Text>
-        );
-      case 'tspan':
-        componentAtts = obtainComponentAtts(node, TEXT_ATTS, fill, fillAll);
-        if (componentAtts.y) {
-          componentAtts.y = getFixedYPosition(node, componentAtts.y);
-        }
-        return (
-          <TSpan key={i} {...componentAtts}>
-            {children}
-          </TSpan>
-        );
-      default:
-        return null;
-    }
+    return (
+      <ElementTag key={`${node.nodeName}-${i}`} {...componentAtts}>
+        {children}
+      </ElementTag>
+    );
   }
 
   inspectNode(node) {
