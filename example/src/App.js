f167ba9861e374df2f956e8f5d362ad1b09d4721
Uilque Messias <messias@work.co>
2020-01-22 11:42:25 -0300

    feat(example): add svg with animation

--- example/src/App.js
+++ example/src/App.js
@@ -6,16 +6,45 @@
  * @flow
  */
 
-import React, { Fragment, useCallback } from 'react';
-import { SafeAreaView, FlatList, View, Text, StatusBar } from 'react-native';
+import React, { Fragment, useCallback, useRef, useEffect } from 'react';
+import {
+  Animated,
+  Easing,
+  SafeAreaView,
+  FlatList,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
 import SVGRenderer from 'react-native-svg-renderer';
 
 import SVGs from './assets/svgs';
 import styles, { Constants } from './styles';
 
+const shouldRotateAny = SVGs.some(({ animate }) => animate === 'rotate');
+const AnimatedSVG = Animated.createAnimatedComponent(SVGRenderer);
+
+const animationDuration = 2500;
+const animationEnd = 360;
+const animationStart = animationEnd / animationDuration;
+
 const App = () => {
+  const rotateAnimation = useRef(new Animated.Value(0)).current;
+  console.warn({ shouldRotateAny });
+  const svgRotationAnimation = shouldRotateAny
+    ? {
+        transform: [
+          {
+            rotate: rotateAnimation.interpolate({
+              inputRange: [0, 1],
+              outputRange: [`${animationStart}deg`, `${animationEnd}deg`],
+            }),
+          },
+        ],
+      }
+    : null;
   const renderItem = useCallback(
-    ({ item: { title, xml, uri }, index }) => (
+    ({ item: { title, xml, uri, animate }, index }) => (
       <View
         style={[
           styles.itemContainer,
@@ -25,18 +54,42 @@ const App = () => {
       >
         <Text style={styles.itemTitle}>{title.toUpperCase()}</Text>
         <View style={styles.svgContainer}>
-          <SVGRenderer
-            svgXmlData={xml}
-            source={!!uri ? { uri } : undefined}
-            height={Constants.cellSize}
-            width={Constants.cellSize}
-          />
+          {!!animate ? (
+            <AnimatedSVG
+              style={svgRotationAnimation}
+              svgXmlData={xml}
+              source={!!uri ? { uri } : undefined}
+              height={Constants.cellSize}
+              width={Constants.cellSize}
+            />
+          ) : (
+            <SVGRenderer
+              svgXmlData={xml}
+              source={!!uri ? { uri } : undefined}
+              height={Constants.cellSize}
+              width={Constants.cellSize}
+            />
+          )}
         </View>
       </View>
     ),
     [SVGs],
   );
   const keyExtractor = useCallback(({ title }) => title, []);
+
+  useEffect(() => {
+    const anim = Animated.loop(
+      Animated.timing(rotateAnimation, {
+        duration: animationDuration,
+        toValue: 1,
+        easing: Easing.linear,
+        useNativeDriver: true,
+      }),
+    );
+
+    anim.start();
+    return anim.stop.bind(anim);
+  });
 
   return (
     <Fragment>
